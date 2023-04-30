ca5af72050752de802bc80cd46cd455728e3f47d
Mateusz Janda <mateusz.janda@gmail.com>
2023-04-30 21:04:11 +0200

    Hello world main

--- main.c
+++ main.c
@@ -1,31 +1,19 @@
-# If KERNELRELEASE is defined, we've been invoked from the
-# kernel build system and can use its language.
-ifneq ($(KERNELRELEASE),)
-    $(info [i]: KERNELRELEASE is set == $(KERNELRELEASE))
+#include <linux/init.h>
+#include <linux/module.h>
 
-    # A makefile symbol used by the kernel build system to determine which modules
-    # should be built in the current directory
-    obj-m := hello_world.o
-    # If module is generated from two source files they can be added here
-    module-objs := hello_world.o
 
-    CFLAGS_hello_world.o := -DDEBUG
+MODULE_LICENSE("Dual BSD/GPL");
 
-# Otherwise we were called directly from the command
-# line; invoke the kernel build system.
-else
-    $(info [i]: KERNELRELEASE is not set)
-    KERNELDIR ?= /lib/modules/$(shell uname -r)/build
-    $(info [i]: KERNELDIR == $(KERNELDIR))
-    PWD := $(shell pwd)
-    $(info [i]: PWD == $(PWD))
 
-.PHONY: modules
-modules:
-	$(info [i]: modules)
-	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
+static int m_init(void)
+{
+	printk(KERN_ALERT "Hello, world\n");
+	return 0;
+}
+static void m_exit(void)
+{
+	printk(KERN_ALERT "Goodbye, cruel world\n");
+}
 
-.PHONY: clean
-clean:
-	$(info [i]: clean)
-	$(MAKE) -C $(KERNELDIR) M=$(PWD) clean
+module_exit(m_exit);
+module_init(m_init);

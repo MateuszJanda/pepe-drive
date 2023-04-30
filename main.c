bfb99fad562aefc3ceaeb5da25d5362c7e959553
Mateusz Janda <mateusz.janda@gmail.com>
2023-04-30 21:13:31 +0200

    Added __init and __exit annotations.

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 
 
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Mateusz Janda");
+MODULE_DESCRIPTION("A simple char device driver that provides ANSI image.");
 
 
-static int m_init(void)
+static int __init m_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
 	return 0;
 }
-static void m_exit(void)
+static void __exit m_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
-module_exit(m_exit);
 module_init(m_init);
+module_exit(m_exit);

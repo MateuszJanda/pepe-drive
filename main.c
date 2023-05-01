165a4bbee668ad77188bfb7e8920f4d28d820831
Mateusz Janda <mateusz.janda@gmail.com>
2023-05-01 19:37:11 +0200

    Changed debug logs

--- main.c
+++ main.c
@@ -45,7 +45,8 @@ ssize_t pepe_read(struct file *filp, char __user *buff, size_t count,
 	ssize_t retval = 0;
 
 	pr_debug("pepe call: %s()\n", __FUNCTION__);
-	pr_debug("pepe_read() request, pos = %lld, count = %lu\n", *f_pos, count);
+	pr_debug("pepe_read() requested, f_pos = %lld, count = %lu\n", *f_pos,
+		 count);
 
 	if (mutex_lock_interruptible(&dev->mutex)) {
 		return -ERESTARTSYS;
@@ -53,6 +54,7 @@ ssize_t pepe_read(struct file *filp, char __user *buff, size_t count,
 
 	if (*f_pos >= sizeof(pepe_data)) {
 		retval = 0;
+		count = 0;
 		goto end_of_file;
 	}
 
@@ -70,7 +72,8 @@ ssize_t pepe_read(struct file *filp, char __user *buff, size_t count,
 
 end_of_file:
 fail_copy_to_user:
-	pr_debug("pepe_read() set, pos = %lld, count = %lu\n", *f_pos, count);
+	pr_debug("pepe_read() delivered, f_pos = %lld, count = %lu\n", *f_pos,
+		 count);
 
 	mutex_unlock(&dev->mutex);
 	return retval;
